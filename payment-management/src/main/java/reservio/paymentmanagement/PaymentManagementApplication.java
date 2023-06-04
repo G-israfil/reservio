5c69a0ae87caac7cb7f19d2b368e9be126c42b2b
G-israfil <israfilgozudeli3@gmail.com>
2023-06-04 16:53:39 +0300

    move services fix build error

---
+++ payment-management/src/main/java/reservio/paymentmanagement/PaymentManagementApplication.java
@@ -0,0 +1,13 @@
+package reservio.paymentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentManagementApplication.class, args);
+    }
+
+}
