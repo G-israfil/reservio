4041415536ee4dd98cbda435cc177419d529e908
G-israfil <israfilgozudeli3@gmail.com>
2023-06-12 23:54:02 +0300

    activate jwt token change spring securtiy to auth0

---
+++ gateway-reservio/src/test/java/reservio/gatewayreservio/GatewayReservioApplicationTests.java
@@ -0,0 +1,13 @@
+package reservio.gatewayreservio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayReservioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
