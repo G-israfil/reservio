2784649b54b9b382f37a614328b08825739aaa6e
G-israfil <israfilgozudeli3@gmail.com>
2023-06-16 15:09:59 +0300

    Dashboard endpoints

--- product-catalog-management/src/main/java/reservio/productcatalogmanagement/productcatalog/messaging/MessageService.java
+++ product-catalog-management/src/main/java/reservio/productcatalogmanagement/productcatalog/messaging/MessageService.java
@@ -5,8 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import reservio.common.queue.events.AccountCreatedEvent;
 import reservio.common.queue.EventType;
+import reservio.common.queue.events.AccountEvent;
 
 
 @Service
@@ -19,7 +19,7 @@ public class MessageService {
     private String queueName;
 
     public void sendAccountCreatedMessage(@NonNull Long accountId){
-        AccountCreatedEvent accountCreatedEvent = new AccountCreatedEvent();
+        AccountEvent accountCreatedEvent = new AccountEvent();
         accountCreatedEvent.setId(accountId);
         rabbitTemplate.convertAndSend(queueName,accountCreatedEvent,message -> {
             message.getMessageProperties().setHeader("x-event-type", EventType.ACCOUNT_CREATED);
